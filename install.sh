91fea4b1034d1e8ba9168f15dc2f6fed6e1ce4e3
jackson457 <66469037+jackson457@users.noreply.github.com>
2020-10-17 18:44:13 +0630

    install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+echo -e "\e[32m 
+
+          _______  _______ 
+|\     /|(  ____ \(  ____ \
+( \   / )| (    \/| (    \/.   writer:jamejackson
+ \ (_) / | (_____ | (_____     ps:I just a beginner
+  ) _ (  (_____  )(_____  ) 
+ / ( ) \       ) |      ) |
+( /   \ )/\____) |/\____) |
+|/     \|\_______)\_______)
+                           
+"
+clear 
+pkg install unstable-repo
+pkg install metasploit
